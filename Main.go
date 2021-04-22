f995b2e32666b3f22e2ecce1c55395e7a90624d6
Fabio Kaufmann <baselzockt@gmail.com>
2021-04-22 12:47:31 +0200

    Änderungen nach codereview

--- Main.go
+++ Main.go
@@ -17,39 +17,44 @@ func setupLogging(loglevel log.Level, consoleOut bool) {
 		return
 	}
 
-	var logfile, err = os.OpenFile("log.txt", os.O_CREATE|os.O_WRONLY, 0666)
+	var logfile, err = os.OpenFile("log.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0640)
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.SetOutput(logfile)
 	log.RegisterExitHandler(func() {
-		_ = logfile.Close()
+		logfile.Close()
 	})
 }
 
 func main() {
+	err := run()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
 	consoleLogging, _ := strconv.ParseBool(os.Getenv("CONSOLELOGGING"))
 	setupLogging(log.DebugLevel, consoleLogging)
-	log.Debug("getting filterstream")
-	filterStreamParams := &twitter.StreamFilterParams{Language: []string{"de"}, Track: []string{"Covid", "impfen"}}
+
 	log.Debug("Creating twitter client")
 	twitterClient := twitterapi.NewTwitterClient(os.Getenv("APIKEY"), os.Getenv("APISECRET"), os.Getenv("ACCESSKEY"), os.Getenv("ACCESSSECRET"))
+
 	log.Debug("Creating activeMq client")
+	impl.StompClient{Url: os.Getenv("ENDPOINT")}
 	activeMqClient := impl.NewStompClient()
 	log.Debug("Connecting to activeMQ endpoint")
 	err := activeMqClient.Connect(os.Getenv("ENDPOINT"))
 
 	if err != nil {
 		log.Error("Could not connect to activeMQ")
-		log.Fatal(err)
+		return err
 	}
 
-	err = twitterapi.CreateHandlerForFilterStream(twitterClient, activeMqClient, filterStreamParams)
+	log.Debug("getting filterstream")
+	filterStreamParams := &twitter.StreamFilterParams{Language: []string{"de"}, Track: []string{"Covid", "impfen"}}
 
-	if err != nil {
-		log.Error("Error while handling Filter stream")
-		log.Fatal(err)
-	}
-
-	log.Debug("Received filter stream")
+	return twitterapi.CreateHandlerForFilterStream(twitterClient, activeMqClient, filterStreamParams)
 }
